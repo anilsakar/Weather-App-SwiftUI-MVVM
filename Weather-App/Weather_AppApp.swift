fe8a8e545a54160976b34da4a450daa0dc01b1b4
anilsakar <anilsakar6@gmail.com>
2020-11-09 03:30:43 -0800

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Anil on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
